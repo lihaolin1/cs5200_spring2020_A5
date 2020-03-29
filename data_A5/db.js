a399c2f5600748a66c822306f439fbd84b2a43f3
Haolin Li <lihaolin8888@gmail.com>
2020-03-29 15:44:19 -0400

    Add files via upload

---
+++ data_A5/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+module.exports = function () {
+    const LOCAL_DB = 'mongodb://localhost:27017/data_test1';
+    const REMOTE_DB = 'URL_TO_YOUR_REMOTE_MONGODB_SERVER';
+    const MONGO_DB = LOCAL_DB;
+    mongoose.connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(u=> console.log("Connected!" + u));
+};
---
+++ data_A5/test.js
@@ -0,0 +1,50 @@
+require('./db')();
+const universityDao = require('./daos/university.dao.server');
+const studentDao = require('./daos/student.dao.server');
+const answerDao = require('./daos/answer.dao.server');
+const questionDao = require('./daos/question.dao.server');
+//const async = require("async");
+//universityDao.truncateDatabase();
+var async = require('async');
+
+/*async.series([
+    //one:  universityDao.truncateDatabase() ,
+    //universityDao.truncateDatabase(),
+    //function (callback) { universityDao.truncateDatabase(); },
+
+    universityDao.populateDatabase(),
+    //two: universityDao.truncateDatabase(),
+
+    universityDao.testStudentInitialCount(),//universityDao.testDeleteStudent(),
+    //function (callback) { universityDao.testStudentInitialCount(); },
+
+    universityDao.testQuestionInitialCount(),//universityDao.testDeleteQuestion(),
+    //function (callback) { universityDao.testQuestionInitialCount(); },
+
+    universityDao.testAnswerInitialCount(),//universityDao.testDeleteAnswer(),
+    //function (callback) { universityDao.testAnswerInitialCount(); },
+
+    universityDao.testDeleteQuestion(),//universityDao.testQuestionInitialCount(),
+
+    //function (callback) { universityDao.testDeleteQuestion(); },
+    universityDao.testDeleteAnswer(),//universityDao.testAnswerInitialCount(),
+
+    //function (callback) { universityDao.testDeleteAnswer(); },
+
+    universityDao.testDeleteStudent(),//universityDao.testStudentInitialCount()
+    //function (callback) { universityDao.testDeleteStudent(); }
+
+    //nine: universityDao.truncateDatabase(),
+    //nine: universityDao.populateDatabase()
+]);*/
+
+
+//universityDao.truncateDatabase();
+
+universityDao.populateDatabase();
+//universityDao.testStudentInitialCount();
+//universityDao.testQuestionInitialCount();
+//universityDao.testAnswerInitialCount();
+//universityDao.testDeleteQuestion();
+//universityDao.testDeleteAnswer();
+//universityDao.testDeleteStudent();
